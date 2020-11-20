9150bb8fdfb95c56d9dd28a1b0f71ebebf4b831c
Brian Pennisi <bp2221@nyu.edu>
2020-11-20 08:26:14 -0500

    added df_to_stats for quick feature ggeneration, added feature helper module, requirements

--- ScriptFeaturizer.py
+++ ScriptFeaturizer.py
@@ -1,46 +1,55 @@
 import os
-import spacy
+import re
+
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from spacy.lang.en.stop_words import STOP_WORDS
-nlp = spacy.load('en_core_web_sm')
 
-'''
-INPUT: a directory containing a .txt file of scripts
-OUTPUT: a vector of textual features
-'''
+from FeatureUtils import tokenize_script
 
-def load_scripts(my_dir):
-    scripts = sorted(os.listdir(my_dir))
-    titles = [file.replace('.txt', '') for file in scripts]
-    raw_scripts = []
-    for file in scripts:
-        file = open(os.path.join(my_dir, file), 'r')
-        raw_scripts.append(file.read())
-    return raw_scripts, titles
 
-# BAG OF WORDS MODEL
-def scripts_to_bow(scripts):
-    
+
+def num_lines(script):
+    """Run regex to determine the number of speaking lines in a script."""
+    return len(re.findall('[A-Z]{3,}[A-Za-z0-9 ]{,20}\\n\ {3,}', script))
+
+
+def scripts_to_tfidf(scripts):
+    """Create Tfidf matrix from tokenized scripts."""
+
     # custom stop words for scripts
     film_stop_words = ['V.O.', "Scene", "CUT TO", "FADE IN"]
     stop_words = STOP_WORDS.union(film_stop_words)
 
-    # vectorize scripts into BoW
+    # vectorize scripts into Tfidf matrix
     vectorizer = TfidfVectorizer(input='content', stop_words=stop_words, min_df = 0.2, ngram_range=(1,1)) # less than 0.2 frequency. bad.
     bow = vectorizer.fit_transform(scripts)
     vocab = vectorizer.get_feature_names()
-    dict = vectorizer.vocabulary_
 
     return bow, vocab
 
-# WORD EMBEDDINGS MODEL
-def script_to_embeddings(directory, filename):
-    features = []
-    # word2vec in here
-    return features
 
-# TRANSFORMERS MODEL
-def script_to_transformer(directory, filename):
+def df_to_stats(df, groupby_col, stat='count'):
+    """Computes a df containing a row for each values to a summary statistic."""
+    if stat == 'count':
+        summary_df = df.groupby(by=groupby_col).count().iloc[:, 0]
+    return summary_df.values, summary_df.index.values
+
+
+def scripts_to_embeddings(scripts):
+    """Create WordEmbedding from tokenized scripts."""
+    
+    docs = [tokenize_script(script, stop_words=True) for script in raw_scripts]
+    
+    model = Word2Vec(docs, 
+                     min_count=100, 
+                     size=100, 
+                     window=5, 
+                     max_vocab_size=2000
+                    )
+    return model
+
+
+def script_to_transformer(scripts):
     features = []
     # pretrained BERT in here
     return features
