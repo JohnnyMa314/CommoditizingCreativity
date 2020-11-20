bec7538dece417e29b82a07fd18b9beb1ecd0ec4
Brian Pennisi <bp2221@nyu.edu>
2020-11-19 20:21:25 -0500

    modularize , add preliminary exploratory notebooks

--- ScriptFeaturizer.py
+++ ScriptFeaturizer.py
@@ -9,26 +9,29 @@ INPUT: a directory containing a .txt file of scripts
 OUTPUT: a vector of textual features
 '''
 
-# BAG OF WORDS MODEL
-def scripts_to_bow(dir):
-    scripts = sorted(os.listdir(dir))
+def load_scripts(my_dir):
+    scripts = sorted(os.listdir(my_dir))
     titles = [file.replace('.txt', '') for file in scripts]
     raw_scripts = []
     for file in scripts:
-        file = open(os.path.join(dir, file), 'r')
+        file = open(os.path.join(my_dir, file), 'r')
         raw_scripts.append(file.read())
+    return raw_scripts, titles
 
+# BAG OF WORDS MODEL
+def scripts_to_bow(scripts):
+    
     # custom stop words for scripts
     film_stop_words = ['V.O.', "Scene", "CUT TO", "FADE IN"]
     stop_words = STOP_WORDS.union(film_stop_words)
 
     # vectorize scripts into BoW
     vectorizer = TfidfVectorizer(input='content', stop_words=stop_words, min_df = 0.2, ngram_range=(1,1)) # less than 0.2 frequency. bad.
-    bow = vectorizer.fit_transform(raw_scripts)
+    bow = vectorizer.fit_transform(scripts)
     vocab = vectorizer.get_feature_names()
     dict = vectorizer.vocabulary_
 
-    return bow, titles, vocab
+    return bow, vocab
 
 # WORD EMBEDDINGS MODEL
 def script_to_embeddings(directory, filename):
